c903bf89d8ef57e2d31a03232e733ab487f24801
BimalPandeyCode <bimalpandey32@gmail.com>
2025-02-28 17:04:41 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,76 @@
+const moment = require("moment");
+const fs = require("fs");
+const {
+  scrapeCompaniesData,
+  scrapeMarketData,
+  fetchData,
+  groupMarketDataByCompany,
+} = require("./scrapper_v3");
+const { lastMarketDay } = require("./helpers");
+const { deleteDownloadedCSV } = require("./helpers/puppet");
+const { scrapeNepseTrading } = require("./scrapper_v4");
+
+// FIX: 'UNABLE_TO_VERIFY_LEAF_SIGNATURE' issue with API call
+process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = "0";
+
+async function runScript() {
+  try {
+    // scrapeNepseTrading()
+    //   .then((stocksArray) => {
+    //     const [data, date] = stocksArray;
+
+    //     if (fs.existsSync(`./data/date/${date}.json`)) return;
+    //     console.log(data, date);
+    //     if (data) {
+    //       scrapeCompaniesData(data);
+    //       scrapeMarketData(data, date);
+    //       groupMarketDataByCompany(data, date);
+    //       console.log("scraped data for", date);
+    //     }
+    //   })
+    //   .catch((err) => {
+    //     console.error("An error occurred:", err);
+    //   });
+
+    //  fetch last 7 market-days data
+
+    let date = moment();
+    for (let i = 0; i <= 10; i++) {
+      try {
+        // TODO : This doesn't seem to fetch today's data so I removed subtract 1, if this is a mistake remove this
+        let dateStr = lastMarketDay(
+          date.subtract(1, "days").format("YYYY-MM-DD")
+        );
+
+        if (fs.existsSync(`./data/date/${dateStr}.json`)) continue;
+        const data = await fetchData(dateStr);
+        if (data) {
+          scrapeCompaniesData(data);
+          scrapeMarketData(data, dateStr);
+          groupMarketDataByCompany(data, dateStr);
+          console.log("scraped data for", dateStr);
+          deleteDownloadedCSV(dateStr);
+        }
+      } catch (e) {
+        console.log(e);
+        continue;
+      }
+    }
+
+    // update info.json
+    fs.writeFileSync(
+      "./data/info.json",
+      JSON.stringify({
+        name: "Nepse API",
+        source: "https://nepalstock.com/",
+        lastUpdatedAt: moment().format("YYYY-MM-DD HH:mm:ss"),
+      })
+    );
+    console.log("SUCCESS");
+  } catch (e) {
+    console.log(e);
+    console.log("FAIL");
+  }
+}
+
+runScript();
